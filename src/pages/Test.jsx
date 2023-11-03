a8d9435c449fd1ac22bf3cddade269f102b7a898
11yhj <uouo_@naver.com>
2023-11-03 14:31:29 +0900

    firebase.jsx 추가

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,19 @@
 const Test = () => {
-    return (
-        <div>
-            <p>
-                테스트페이지 입니다.
-                <ul>
-                    <li>아아!</li>
-                </ul>
-            </p>
-        </div>
-    )
+  let post = "어쩌구 저쩌구";
+
+  return (
+    <div className="App">
+      <div className="gray-nav">
+        <h4 id={post}>상단바예시</h4>
+      </div>
+      <div>
+        <h2>메뉴</h2>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
 };
+
 export default Test;
+
+// gray-nav <- css 부분임
